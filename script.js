47a563e7e7fb4fb68486a9ccb7a88bf5534d4d05
shivendra0712 <patelshivendra817@gmail.com>
2024-12-27 22:53:20 +0530

    2 question done

--- script.js
+++ script.js
@@ -5,3 +5,13 @@
 // let a =4;
 // let b =5;
 // console.log(a+b); 
+
+// 2- Print the messege along with values and sum
+// Ex - The sum of 45 & 12  = 57
+
+// let a =45;
+// let b =12;
+//  console.log(` The sum of 45 & 12  = ${a+b}`); 
+// console.log(" The sum of 45 & 12  = " +(a+b)); 
+// console.log(" The sum of 45 & 12  = " ,(a+b)); 
+
