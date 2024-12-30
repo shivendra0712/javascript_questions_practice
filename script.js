aa5d44d845a5dfe6c0f8e69e491a97946f9973b6
shivendra0712 <patelshivendra817@gmail.com>
2024-12-30 18:44:11 +0530

    4 question done

--- script.js
+++ script.js
@@ -6,6 +6,9 @@
 // let b =5;
 // console.log(a+b); 
 
+// output:
+//  9
+
 // 2- Print the messege along with values and sum
 // Ex - The sum of 45 & 12  = 57
 
@@ -15,14 +18,30 @@
 // console.log(" The sum of 45 & 12  = " +(a+b)); 
 // console.log(" The sum of 45 & 12  = " ,(a+b)); 
 
+// output:
+//   The sum of 45 & 12 = 57
+
 // 3- Accept two integers from user and print the sum
 // 	Ex - The sum of 45 & 12 = 57
 
-let num1 = +prompt("enter num1 :");
-let num2 = +prompt("enter num2 :");
+// let num1 = +prompt("enter num1 :");
+// let num2 = +prompt("enter num2 :");
 
-console.log(` The sum of 45 & 12  = ${num1+num2}`);
+// console.log(` The sum of 45 & 12  = ${num1+num2}`);
 
+// output:
+//   The sum of 45 & 12 = 57
+
+// 4- Accept the User's name, age and print in following manner
+// 	Ex - Hello Shery, you are 12 years old.
+
+let name = prompt("enter your name :");
+let age = +prompt("enter your age :");
+
+console.log(` Hello ${name}, you are ${age} years old`);
+
+// output:
+// Hello Sherya, you are 12 years old
 
 
 
