27b340c417a3941e3e53e1e39244c1fc7bbe1e7e
shivendra0712 <patelshivendra817@gmail.com>
2024-12-31 13:11:42 +0530

    31 question node

--- script.js
+++ script.js
@@ -423,13 +423,13 @@
 // 30- Print all the factors of a number.
 
 
-let n = +prompt("enter an number :");
+// let n = +prompt("enter an number :");
 
-for(let i =1 ; i<=n ;i++){
-    if(n%i == 0){
-        console.log(i);
-    }
-}
+// for(let i =1 ; i<=n ;i++){
+//     if(n%i == 0){
+//         console.log(i);
+//     }
+// }
 
 // output
 //  n=10
@@ -437,4 +437,24 @@ for(let i =1 ; i<=n ;i++){
 // 2
 // 5
 // 10
+
+// 31- Print the sum of all factors of a number, 50 -> 1 + 2 + 5 + 10 + 25 = 43
+
+
+let n = +prompt("enter an number :");
+
+let sum =0;
+for(let i =1 ; i<n ;i++){
+    if(n%i == 0){
+        sum += i;
+       
+    }
+}
+console.log("sum of all factors of a number",sum);
+// output
+//  n=50
+// sum of all factors of a number 43
+
+
+
 
